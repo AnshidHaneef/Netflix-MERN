cc081e38618fccf81bf4ac8de78964d5ddb77737
anshid haneef <anshidhaneefa710@gmail.com>
2023-05-06 23:49:36 +0530

    second commit

---
+++ client/src/components/list/List.jsx
@@ -0,0 +1,12 @@
+import './list.scss'
+
+export default function List() {
+  return (
+    <div className='list'>
+        <div className="title">
+            Now Trending
+        </div>
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+    </div>
+  )
+}
--- client/src/components/rowPost/RowPost.js
+++ client/src/components/rowPost/RowPost.js
@@ -3,8 +3,23 @@ import './rowPost.scss'
 export default function RowPost() {
   return (
     <div className='row'>
-       
-        
+       <span>Trending</span>
+      <div className="posters">
+      <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+      </div> 
     
     </div>
   )
