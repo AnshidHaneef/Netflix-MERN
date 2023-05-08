ee84ddb3e7e04b80ccb21b456b2b7482a5ce7857
anshid haneef <anshidhaneefa710@gmail.com>
2023-05-08 23:51:43 +0530

    styled listItems and listed components

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,48 +1,60 @@
-import {  ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
-import './list.scss'
-import ListItem from '../listItem/ListItem'
-import { useRef, useState } from 'react'
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@mui/icons-material";
+import "./list.scss";
+import ListItem from "../listItem/ListItem";
+import { useRef, useState } from "react";
 
 export default function List() {
-const [slideNumber,setSlideNumber] = useState(0)
-const listRef = useRef()
+  const [moved, isMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const listRef = useRef();
 
-const handleClick= (direction )=>{
-  let distance = listRef.current.getBoundingClientRect().x - 50
-  if(direction === 'left' && slideNumber > 0){
-    listRef.current.style.transform = `translateX( ${230+ distance }px)`
-    setSlideNumber(slideNumber - 1)
-  }
-  if(direction === 'right' && slideNumber <5 ){
-    listRef.current.style.transform = `translateX( ${-230+ distance }px)`
-    setSlideNumber(slideNumber + 1)
+  const handleClick = (direction) => {
+    isMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      listRef.current.style.transform = `translateX( ${188 + distance}px)`;
+      setSlideNumber(slideNumber - 1);
+    }
+    if (direction === "right" && slideNumber < 4 ) {
+      listRef.current.style.transform = `translateX( ${-188 + distance}px)`;
+      setSlideNumber(slideNumber + 1);
+    }
+  };
 
-  }
-}
+  console.log(slideNumber ,'slideNumber');
+  console.log(setSlideNumber,'setSlidenumber');
 
   return (
-    <div className='list'>
+    <div className="list">
       <span className="listTitle">Trending Now</span>
-      
-        <div className="wrapper">
-        <ArrowBackIosOutlined className='arrowIcon left' onClick={ ()=>handleClick('left') } />
-        <div className="container" ref={listRef}  >
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/> 
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
+
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          className="arrowIcon left"
+          onClick={() => handleClick("left")}
+          style={{ display: !moved && slideNumber === 0 && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
         </div>
-        <ArrowForwardIosOutlined className='arrowIcon right' onClick={ ()=>handleClick('right') }/>
-        </div>
-   
-   
+        <ArrowForwardIosOutlined
+          className="arrowIcon right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
     </div>
-  )
+  );
 }
--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -1,14 +1,38 @@
-import './listItem.scss'
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from "@mui/icons-material";
+import "./listItem.scss";
 
 export default function ListItem() {
   return (
-    <div>
-        <div className="listItem">
-        
-        {/* List  */}
-        <img src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+    <>
+      <div className="listItem">
+        <img
+          src="https://www.themoviedb.org/t/p/original/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg"
+          alt="poster"
+        />
+      </div>
 
+        <div className="listInfo">
+          <div className="infoIcons">
+            <PlayArrow />
+            <Add />
+            <ThumbUpAltOutlined />
+            <ThumbDownAltOutlined />
+          </div>
+          <div className="infoTexts">
+            <span>1 hr 45 min</span>
+            <span>+16</span>
+            <span>1999</span>
+          </div> 
+          <div className="desc">
+            Lorem ipsum dolor sit
+          </div>
+         <div className="genere"></div>
         </div>
-    </div>
-  )
+    </>
+  );
 }
