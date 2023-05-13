c2082e0c4f5cd23a3212664d50619a9bb5027c5f
anshid haneef <anshidhaneefa710@gmail.com>
2023-05-13 23:56:55 +0530

    models added

---
+++ server/models/Movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const movieSchema = new mongoose.Schema(
+    {
+        movieName: {equired:true , type:String , unique:true },
+        desc: {type:String  },
+        image: { type:String  },
+        imageTitle: { type:String , default:'' },
+        smallImage: { type:String  },
+        trailer: { type: String   },
+        video: { type:String  },
+        year: { type:String  },
+        limit: { type:String  },
+        genre: { type:String  },
+        isSeries: { type:Boolean , default: false  }
+    },
+    {timestamps:true}
+)
+
+module.exports = mongoose.model('movie',movieSchema)
