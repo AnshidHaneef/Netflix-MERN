0ce9bcc4d3f2ec4e91c26c32515704080ca9b3d9
anshid haneef <anshidhaneefa710@gmail.com>
2023-05-13 23:51:28 +0530

    edited

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected!'));
 
 app.listen(3000,()=>{
     console.log('server running on port http://localhost:3000');
