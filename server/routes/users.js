d056c4a94b1b6b48b06e33c9453d4f538b468479
anshid haneef <anshidhaneefa710@gmail.com>
2023-06-08 22:48:05 +0530

    Rest api for User access and user status

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ const verify = require('../verifyToken')
 
 // Update
 router.put('/:id',verify, async (req, res) => {
+    console.log('req.user',req.user);
 
     if (req.user.id === req.params.id || req.user.isAdmin) {
         if (req.body.password) {
@@ -17,7 +18,6 @@ router.put('/:id',verify, async (req, res) => {
             res.status(200).json(updateUser)
 
         } catch (error) {
-            console.log('errorrrrrrrrrrr$$$$###',error);
             res.status(500).json(error)
         }
     } else {
@@ -26,11 +26,46 @@ router.put('/:id',verify, async (req, res) => {
 })
 
 
-
 // Delete
 
-// Get
+router.delete('/:id',verify, (req,res)=>{
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+             User.findByIdAndDelete(req.params.id)
+            res.status(200).json('User deleted successfully')
+
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(403).json('you can delete only your account')
+    }
+})
+
+// Get By Id 
+router.get('/:id',verify, async(req,res)=>{
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        try {
+            const user = await User.findById(req.params.id)
+            res.status(200).json(user)
+        } catch (error) {
+            res.status(401).json('cannot find User')
+        }
+    }
+})
+
 // Get All
+router.get('/',async (req,res)=>{
+        try {
+          const userss =   await User.find()
+            res.status(200).json(userss)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+})
+
 // Get User Stats 
+
+
 
 module.exports = router
