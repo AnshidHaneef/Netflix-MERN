84da722c088575fba0f77d9939587e7f4a9b9125
anshid haneef <anshidhaneefa710@gmail.com>
2023-06-13 22:22:33 +0530

    edited

--- server/routes/lsits.js
+++ server/routes/lsits.js
@@ -1,11 +1,9 @@
 const router = require('express').Router()
-const jwt = require('jsonwebtoken');
 const Lists = require('../models/List');
 const verify = require('../verifyToken');
-const List = require('../models/List');
 
 // Create List 
-router.post('/',verify , (req,res)=>{
+router.post('/', verify, (req, res) => {
     if (req.user.isAdmin) {
         const newList = new Lists(req.body)
         try {
@@ -15,51 +13,51 @@ router.post('/',verify , (req,res)=>{
             res.status(500).json(error)
         }
     } else {
-      res.status(403).json('you are not allow to create List')  
+        res.status(403).json('you are not allow to create List')
     }
 })
 
 
 // Get lists 
-router.get('/find',async(req,res)=>{
-    const typeQuery = req.query.type
-    const genreQuery = req.query.genre
+router.get("/find", verify, async (req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
     let list = [];
     try {
         if (typeQuery) {
-            if(genreQuery){
-                list = await Lists.aggregate([{
-                    $sample:{size:5},
-                    $match:{type:typeQuery,genre:genreQuery}
-                }])
-            }else{
-                list = await Lists.aggregate([{
-                    $sample:{size:5},
-                    $match:{type:typeQuery}
-                }])
-            }  
+            if (genreQuery) {
+                list = await Lists.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery, genre: genreQuery } },
+                ]);
+            } else {
+                list = await Lists.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery } },
+                ]);
+            }
         } else {
-            list = await Lists.aggregate([{$sample:{size:5}}])
+            list = await Lists.aggregate([{ $sample: { size: 10 } }]);
         }
-            res.status(200).json(list)
-    } catch (error) {
-        res.status(500).json(error)
+        res.status(200).json(list);
+    } catch (err) {
+        res.status(500).json(err);
     }
-})
+});
 
 
 
 // Delete List
-router.delete('/:id',verify,async(req,res)=>{
+router.delete('/:id', verify, async (req, res) => {
 
-    if(req.user.isAdmin){
+    if (req.user.isAdmin) {
         try {
-           await  Lists.findByIdAndDelete(req.params.id)
+            await Lists.findByIdAndDelete(req.params.id)
             res.status(200).json('list deleted Successsfully')
         } catch (error) {
             res.status(500).json(error)
         }
-    }else{
+    } else {
         req.status(403).json('Not able to delete ...')
     }
 })
