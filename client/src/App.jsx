6630e241996ec0caee618239c491615c58268a3b
anshid haneef <anshidhaneefa710@gmail.com>
2023-06-14 23:43:35 +0530

    router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import './app.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Login from './pages/Login/Login';
-import Watch from './pages/Watch/Watch'
+// import Watch from './pages/Watch/Watch'
 import Register from './pages/signup/Register';
 import Home from './pages/Home/Home';
 
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      {/* <Watch />  */}
-    {/* <Register/> */}
-    {/* <Login/> */}
-    </div>
+    
+<Router>
+<Routes>
+<Route exact path='/' element={ <Home/> } />
+<Route exact path='/login' element={ <Login/> } />
+<Route exact path='/register' element={ <Register/> } />
+</Routes>
+</Router>
+
   );
 }
 
